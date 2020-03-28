e7d9db7f5cf7938ea7e93046fe7336d268f9923d
Meghna Dash <meghna@Meghnas-MacBook-Pro.local>
2020-03-28 02:12:01 -0400

    showing up on right click

---
+++ public/events.js
@@ -0,0 +1,6 @@
+var contextMenuItem = {
+  "id": "verifyInformation",
+  "title": "Verify Information",
+  "contexts": ["selection"]
+};
+chrome.contextMenus.create(contextMenuItem);
--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import logo from './icon.png';
 import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <img src={logo} alt="logo" />
-      panda panda panda
+      <h1>
+        Coronovirus Fact-Checking
+      </h1>
+      <div class="progress">
+        <div class="progress-bar" />
+      </div>
+      <h2>
+        Fighting the spread of misinformation.
+      </h2>
+
     </div>
   );
 }
