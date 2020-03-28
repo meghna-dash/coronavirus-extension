9be6ba6e2df4ca860775f4be970eb356b881c02a
Meghna Dash <meghna@Meghnas-MacBook-Pro.local>
2020-03-28 03:31:20 -0400

    calls to api working

--- public/events.js
+++ public/events.js
@@ -5,9 +5,29 @@ var contextMenuItem = {
 };
 chrome.contextMenus.create(contextMenuItem);
 
+function query(text) { 
+  var body = { 
+    "query": text
+  } 
+  const url = "https://covid-api.arjungandhi.com/"
+  fetch(url,
+    { 
+      method: "POST",
+      headers: { 
+        "Accept": "application/json",
+        "Content-Type": "application/json" 
+    }, 
+    body: JSON.stringify(body)
+  })
+  .then(res => { return res.json() })
+  .then(e => alert(JSON.stringify(e)))
+  .catch(err => { 
+    alert(err)
+  }) 
+} 
+
 chrome.contextMenus.onClicked.addListener(function(clickData){
-  console.log("WHOOHOOO", clickData)
   if (clickData.menuItemId == "verifyInformation" && clickData.selectionText){
-    alert(clickData.selectionText)
+    query(clickData.selectionText)
   }
 })
